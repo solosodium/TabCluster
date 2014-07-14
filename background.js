8b8a116162e0e30d9787f2c7583ef66c1a05d41f
Hao Fu <fuhaoee@gmail.com>
2014-07-14 11:02:15 -0400

    List all tabs with favicon and title
    href pointing to the url, but not working

--- background.js
+++
@@ -1,6 +0,0 @@
-// deligate function when a link is clicked
-function onTabClick (id) {
-  chrome.tabs.update(id, {hightlighted: true}, function(tab) {
-    // noting
-  });
-}
--- list.js
+++ list.js
@@ -1,28 +1,18 @@
 $(document).ready(function(){
 
-  var len;
-
   // dedicate function to get all tabs
   function getTabs () {
     // query all tabs
     chrome.tabs.query({currentWindow: true}, function(tabs) {
       // iterate through tabs
       for (var k=0; k<tabs.length; k++) {
-        $("#list").append("<p><a href='#' id='" + k + "'>" + tabs[k].title + "</a></p>");
+        $("#list").append("<p><img src='" + tabs[k].favIconUrl + "' />");
+        $("#list").append("<a href='" + tabs[k].url + "' id='" + k + "'>" + tabs[k].title + "</a></p>");
       }
       len = tabs.length;
     });
   }
 
-  // set tab click event
-  function setTabOnClick() {
-    for (var k=0; k<len; k++) {
-      console.debug(k);
-    }
-  }
-
   // main function
-  getTabs();
-  setTabOnClick();
-
+  getTabs();ß
 });
